216f9f056f4791164b455216995f3cd53018c27b
orianetioni <79125296+orianetioni@users.noreply.github.com>
2021-05-31 15:33:04 +1100

    theme

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'title-tag' );
+
+    function add_theme_scripts() {
+        wp_enqueue_style( 'style', get_stylesheet_uri() );
+       
+        wp_enqueue_style( 'custom', get_template_directory_uri() . '/assets/css/custom.css', array(), '1.1', 'all');
+       
+        wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/custom.js', array ( 'jquery' ), 1.1, true);
+       
+      }
+      add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+
+      function wpb_custom_new_menu() {
+        register_nav_menu('my-custom-menu',__( 'My Custom Menu' ));
+      }
+      add_action( 'init', 'wpb_custom_new_menu' );
+
+      function wpb_init_widgets_custom($id) {
+        register_sidebar( array(
+          'id' => 'sidebar',
+          'name' => 'yolo',
+          'before_widget'  => '<div class="site__sidebar__widget %2$s">',
+          'after_widget'  => '</div>',
+          'before_title' => '<p class="site__sidebar__widget__title">',
+          'after_title' => '</p>',
+        ) );
+    }
+    add_action('widgets_init','wpb_init_widgets_custom');    
