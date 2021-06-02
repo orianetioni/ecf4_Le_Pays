a0d9c7ac45b63a483258a299d71f6a0a8649f31d
orianetioni <79125296+orianetioni@users.noreply.github.com>
2021-06-03 10:36:47 +1100

    update

--- front-page.php
+++ front-page.php
@@ -3,21 +3,19 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col">
-	        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    		<div class="col-md-auto">
+	       
+		   	 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    		
+				<div class="col-md-auto">
 					<h1><?php the_title(); ?></h1>
-			</div>
-			<div class="col-md-8">
 					<?php the_content(); ?>
-			<div class="col-md-4">
 					<?php the_post_thumbnail(); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 	
-<?php the_excerpt(); ?>
-</div>
-	</div>
-	</div>
-	</div>
+
 
 <?php endwhile; endif; ?>
      
--- header.php
+++ header.php
@@ -1,28 +1,24 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <?php wp_head(); ?>
-</head>
+    <!DOCTYPE html>
+    <html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <?php wp_head(); ?>
+    </head>
 
-   <body class="home blog logged-in admin-bar no-customize-support" <?php body_class(); ?>>
- 
-
+    <body class="home blog logged-in admin-bar no-customize-support" <?php body_class(); ?>>
+    
     <header class="header">
-    <?php 
+        <?php 
 
 
-    wp_nav_menu ( array (
-    'theme_location' => 'menu' ,
-    'menu_class' => 'menu', 
-    ) ); ?>
-   <a href="<?php echo home_url( '/' ); ?>">    
-    <img class="img_header" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
-     </a>
-    
-  
-      
+        wp_nav_menu ( array (
+        'theme_location' => 'menu' ,
+        'menu_class' => 'menu', 
+        ) ); ?>
+    <a href="<?php echo home_url( '/' ); ?>">    
+        <img class="img_header" src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+        </a>
     </header>
-    
-    <?php wp_body_open(); ?>
+        
+        <?php wp_body_open(); ?>
